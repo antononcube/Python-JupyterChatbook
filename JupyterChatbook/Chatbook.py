5e1cc08e0502db3dae8d2cafc98d3a919aa24fa5
antononcube <antononcube@posteo.net>
2023-09-29 15:02:13 -0400

    feat:Implemented DALL-E image generation cell

--- JupyterChatbook/Chatbook.py
+++ JupyterChatbook/Chatbook.py
@@ -2,6 +2,9 @@ from IPython.core.magic import (Magics, magics_class, line_magic, cell_magic)
 from IPython.core.magic_arguments import (argument, magic_arguments, parse_argstring)
 from LLMFunctionObjects import llm_synthesize, llm_configuration, llm_evaluator, llm_function, llm_chat
 from LLMPrompts import llm_prompt
+import openai
+import IPython
+from base64 import b64decode
 
 
 def unquote(v):
@@ -13,6 +16,62 @@ def unquote(v):
 @magics_class
 class Chatbook(Magics):
     chatObjects = {"NONE": llm_chat('', llm_evaluator='ChatGPT')}
+    dallESizes = {"small": "256x256", "medium": "512x512", "large": "1024x1024",
+                  "256": "256x256", "512": "512x512", "104": "1024x1024"}
+
+    @magic_arguments()
+    @argument('-s', '--size', type=str, default="small", help="Size of the generated image")
+    @argument('-n', type=int, default=1, help="Number of generated images")
+    @argument('-f', '--response_format', type=str, default="b64_json", help='Format, one of "url" or "b64_json"')
+    @argument('--api_key', type=str, help="API key to access the LLM service")
+    @cell_magic
+    def dalle(self, line, cell):
+        """
+        OpenAI DALL-E magic for image generation by prompt.
+        :return: Image.
+        """
+        args = parse_argstring(self.dalle, line)
+        args = vars(args)
+        args = {k: unquote(v) for k, v in args.items()}
+
+        # Size
+        size = args.get("size", "small")
+        if size not in self.dallESizes:
+            expectedValues = list(self.dallESizes.keys()) + [self.dallESizes["small"], self.dallESizes["medium"],
+                                                             self.dallESizes["large"]]
+            print(f'The size argument expects a value that is one of: {expectedValues}. Using "256x256".')
+
+        size = self.dallESizes.get(size, "256x256")
+
+        # Response format
+        resFormat = args.get("response_format", "b64_json")
+        if resFormat not in ["url", "b64_json"]:
+            print(f'The response_format argument expects a value that is one of: "url", "b64_json". Using "b64_json".')
+            resFormat = "url"
+
+        # Call to OpenAI
+        res = openai.Image.create(
+            prompt=cell,
+            n=args["n"],
+            size=size,
+            response_format=resFormat
+        )
+
+        # Post process results
+        if resFormat == "url":
+            new_cell = [x["url"] for x in res["data"]]
+            new_cell = f'print("{str(new_cell)}")'
+        else:
+            bImages = []
+            for b in res["data"]:
+                d = b["b64_json"]
+                img = f"<img src=\"data:image/png;base64,{d}\" />"
+                bImages.append(img)
+
+            new_cell = "import IPython\nIPython.display.HTML('" + ''.join(bImages) + "')"
+
+            # Result
+        self.shell.run_cell(new_cell)
 
     @magic_arguments()
     @argument('-i', '--chat_id', default="NONE", help="Identifier (name) of the chat object")
