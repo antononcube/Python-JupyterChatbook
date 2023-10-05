8ce815c67fe32cc980a2134b1b06e6fcc0dbab3c
antononcube <antononcube@posteo.net>
2023-10-04 20:53:28 -0400

    build:Micro version increment.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="JupyterChatbook",
-    version="0.0.8",
+    version="0.0.9",
     author="Anton Antonov",
     author_email="antononcube@posteo.net",
     description="Custom Jupyter magics for interacting with LLMs.",
