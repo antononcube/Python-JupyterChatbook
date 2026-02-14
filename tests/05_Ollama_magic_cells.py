d3962edaee053b3b4f1bcd4fadf05099b4b42083
antononcube <antononcube@posteo.net>
2026-02-14 09:37:05 -0500

    test:First version of Ollama magic cell tests.

---
+++ tests/05_Ollama_magic_cells.py
@@ -0,0 +1,39 @@
+import unittest
+from LLMPrompts.LLMPrompts import llm_prompt
+import IPython.core.interactiveshell as ici
+
+
+class OllamaCellsTests(unittest.TestCase):
+
+    def test_01_ollama_cells(self):
+        ish = ici.InteractiveShell()
+        cells = [
+            "%load_ext JupyterChatbook",
+            "%%ollama" + """
+            How many people live in Portugal?
+            """,
+            "%%ollama" + """
+            Write a funny story in Russian
+            """
+        ]
+
+        for c in cells:
+            er = ish.run_cell(c)
+            self.assertTrue(er.success)
+
+    def test_02_ollama_cells(self):
+        ish = ici.InteractiveShell()
+        cells = [
+            "%load_ext JupyterChatbook",
+            "%%ollama" + """
+            Write a funny story in Russian.
+            """
+        ]
+
+        for c in cells:
+            er = ish.run_cell(c)
+            self.assertTrue(er.success)
+
+
+if __name__ == '__main__':
+    unittest.main()
