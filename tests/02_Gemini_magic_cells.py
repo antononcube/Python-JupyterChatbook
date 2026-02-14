9cdfebfbbd58aa04ccbd38513f8d48713b4e5c5c
antononcube <antononcube@posteo.net>
2026-02-14 09:08:10 -0500

    test:Changed PaLM tests to Gemini tests.

--- tests/02_Gemini_magic_cells.py
+++ tests/02_Gemini_magic_cells.py
@@ -3,13 +3,13 @@ from LLMPrompts.LLMPrompts import llm_prompt
 import IPython.core.interactiveshell as ici
 
 
-class PaLMCellsTests(unittest.TestCase):
+class GeminiCellsTests(unittest.TestCase):
 
-    def test_01_palm_cells(self):
+    def test_01_gemini_cells(self):
         ish = ici.InteractiveShell()
         cells = [
             "%load_ext JupyterChatbook",
-            "%%palm" + """
+            "%%gemini" + """
             How many people live in Portugal?
             """,
             "%%palm" + """
@@ -21,11 +21,11 @@ class PaLMCellsTests(unittest.TestCase):
             er = ish.run_cell(c)
             self.assertTrue(er.success)
 
-    def test_02_palm_cells(self):
+    def test_02_gemini_cells(self):
         ish = ici.InteractiveShell()
         cells = [
             "%load_ext JupyterChatbook",
-            "%%palm" + """
+            "%%gemini" + """
             Write a funny story in Russian.
             """
         ]
