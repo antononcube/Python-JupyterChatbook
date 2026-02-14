c617327e06191ff95e66a473c42fcc8924730c1f
antononcube <antononcube@posteo.net>
2026-02-13 21:01:15 -0500

    build:Micro version increment. New versions dependencies.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="JupyterChatbook",
-    version="0.0.17",
+    version="0.0.18",
     author="Anton Antonov",
     author_email="antononcube@posteo.net",
     description="Custom Jupyter magics for interacting with LLMs.",
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/antononcube/Python-JupyterChatbook",
     packages=setuptools.find_packages(),
-    install_requires=["LLMFunctionObjects>=0.1.5", "LLMPrompts>=0.1.4",
+    install_requires=["LLMFunctionObjects>=0.2.0", "LLMPrompts>=0.2.0",
                       "IPython>=8.15.0",
                       "pyperclip>=1.8.2",
                       "google-generativeai>=0.2.0",
