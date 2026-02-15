b2951eb9763d807c6a03e94bb04cd743a668981b
antononcube <antononcube@posteo.net>
2026-02-14 19:23:15 -0500

    docs:Example init files.

---
+++ resources/init.py
@@ -0,0 +1,16 @@
+#from ExampleDatasets import *
+#from LatentSemanticAnalyzer import *
+from SparseMatrixRecommender import *
+from Regressionizer import *
+from SSparseMatrix import *
+from OutlierIdentifiers import *  
+#from RandomDataGenerators import *
+
+from DSLExamples import *
+from LLMTextualAnswer import *
+
+#from ChernoffFace import *
+from RandomMandala import *
+from DSLTranslation import *
+
+import random
